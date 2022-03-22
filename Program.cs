050ded56411d27253a26d62b03048a37500b2092
Blake Laprade <96034935+thePumpkinKing8@users.noreply.github.com>
2022-03-22 12:55:19 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +6,49 @@ using System.Threading.Tasks;
 
 namespace Connect_4
 {
-    class GameBoard
-    {
+    
+
+    class Gameboard
+    { 
+        string[,] board = new string[6,7]; // represents the  values of each space on the board
+        public Gameboard() // constructs an empty gameboard
+        {
+            for(int i=0;i<=5;i++)
+            {
+                for (int f = 0; f <= 6; f++)
+                {
+                    board[i,f] = " ";
+                }
+            }
+        }
+
+        public void Display()  // displays the board and game UI
+        {
+            Console.WriteLine("  1    2    3    4    5    6    7");
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int f = 0; f <= 6; f++)
+                {
+                    
+                    Console.Write(" [" + board[i, f] + "] ");
+                }
+                Console.Write("\n \n");
+                
+            }
+            return;
+        }
 
     }
+
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Gameboard gameboard = new Gameboard();
+            gameboard.Display();
+            Console.ReadKey();
+            
         }
     }
 }
