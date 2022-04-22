b60eb6833ea9b6c66fb909a6d8a2d543aeb84b7f
Blake Laprade <96034935+thePumpkinKing8@users.noreply.github.com>
2022-04-22 15:06:02 -0600

    added player class, Ai class and polished gameplay

--- Program.cs
+++ Program.cs
@@ -10,299 +10,387 @@ namespace Connect_4
 {
 
 
+    class Player
+    {
+        public char piece;
+        
+        public Player()
+        {
 
-class GameBoard
-{
-const int rows = 6, columns = 7;
-const char Empty = '0', PLAYER1 = 'X', PLAYER2 = 'O';
+        }
+        public Player(char pvalue)
+        {
+            piece = pvalue;
+            
+        }
 
 
+        public virtual String PlayerInput(char[,] board) //player inputs value
+        {
+            return Console.ReadLine();
+        }
+    
+    }
 
-private char[,] board;
+    class Ai : Player
+    {
 
+        public Ai(char pvalue)
+        {
+            piece = pvalue;
+            
+        }
+        public override String PlayerInput(char[,] board)    //Ai inputs value
+        {
+            String choice;
+            var rand = new Random();
+            choice = (rand.Next(7)).ToString();
+            if (board[0, Int32.Parse(choice)] != ' ')
+            {
 
+            }
+                
+            Console.WriteLine("Ai places piece in column " + choice);
+            return choice;
 
-int Count;
+        }
 
+        
 
 
 
-public GameBoard()
-{
-board = new char[rows, columns];
+    }
 
 
 
-for (int y = 0; y < rows; y++)
-for (int x = 0; x < columns; x++)
-board[y, x] = Empty;
-}
 
 
 
-public void Display()
-{
-for (int i = 0; i < rows; i++)
-{
-for (int f = 0; f < columns; f++)
-{
-Console.Write(" [" + board[i, f] + "] ");
-}
-Console.Write("\n \n");
+    class GameBoard
+    {
+        const int rows = 6, columns = 7;
+        const char Empty = ' ', PLAYER1 = 'X', PLAYER2 = 'O'; // empty space makes board easier to see than a 0
 
 
 
-}
-return;
-}
+          internal char[,] board;
 
 
 
+        int Count;
 
-public bool DropPiece(char player, int column)
-{
-column--;
 
 
 
-if (board[0, column] != Empty)
-return false;
+        public GameBoard()
+        {
+          board = new char[rows, columns];
 
 
 
-for (int y = 0; y < rows; y++)
-{
-if ((y == rows - 1) || (board[y + 1, column] != Empty))
-{
-board[y, column] = player;
-break;
-}
-}
-Count++;
-return true;
-}
+            for (int y = 0; y < rows; y++) //sets every point on the array to Empty
+                for (int x = 0; x < columns; x++)
+                    board[y, x] = Empty;
+        }
 
 
 
-public bool connectRow(char player)
-{
-// Horizontal check:
+        public void Display()         //displays the game board
+        {
+            Console.WriteLine("  1    2    3    4    5    6    7");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int f = 0; f < columns; f++)
+                {
+                    Console.Write(" [" + board[i, f] + "] ");
+                }
+                Console.Write("\n \n");
+            }
+            return;
+        }
 
 
 
-for (int y = 0; y < rows; y++)
-for (int x = 0; x < 4; x++)
-if (board[y, x] == player && board[y, x + 1] == player)
-if (board[y, x + 2] == player && board[y, x + 3] == player)
-return true;
 
+        public bool DropPiece(char player, int column) //checks to see if the chosen column is full then places the players piece into the board
+        {
+            column--;
 
 
-// Vertical check:
 
+            if (board[0, column] != Empty)
+                return false;
 
 
-for (int y = 0; y < 3; y++)
-for (int x = 0; x < columns; x++)
-if (board[y, x] == player && board[y + 1, x] == player)
-if (board[y + 2, x] == player && board[y + 3, x] == player)
-return true;
 
+            for (int y = 0; y < rows; y++)
+            {
+                if ((y == rows - 1) || (board[y + 1, column] != Empty))
+                {
+                    board[y, column] = player;
+                    break;
+                }
+            }
+            Count++;
+            return true;
+        }
 
 
-// Diagonal check:
 
+        public bool connectRow(char player) //checks how many pieces of the same type are in a row horizontally, vertically and diagonally 
+        {
+            // Horizontal check:
 
 
-for (int y = 0; y < 3; y++)
-{
-for (int x = 0; x < columns; x++)
-{
 
+            for (int y = 0; y < rows; y++)
+                for (int x = 0; x < 4; x++)
+                    if (board[y, x] == player && board[y, x + 1] == player)
+                        if (board[y, x + 2] == player && board[y, x + 3] == player)
+                            return true;
 
 
-if (board[y, x] == player)
-{
 
+            // Vertical check:
 
 
-// Diagonally left:
-try
-{
-if (board[y + 1, x - 1] == player)
-{
-if (board[y + 2, x - 2] == player)
-if (board[y + 3, x - 3] == player)
-return true;
-}
-}
-catch (IndexOutOfRangeException) { }
 
+            for (int y = 0; y < 3; y++)
+                for (int x = 0; x < columns; x++)
+                    if (board[y, x] == player && board[y + 1, x] == player)
+                        if (board[y + 2, x] == player && board[y + 3, x] == player)
+                            return true;
 
 
-// Diagonally right:
-try
-{
-if (board[y + 1, x + 1] == player)
-{
-if (board[y + 2, x + 2] == player)
-if (board[y + 3, x + 3] == player)
-return true;
-}
-}
-catch (IndexOutOfRangeException) { }
-}
-}
-}
 
+            // Diagonal check:
 
 
-return false;
-}
 
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
 
 
-public bool Full()
-{
-return Count >= rows * columns;
-}
-}
 
+                    if (board[y, x] == player)
+                    {
 
 
 
-/*class Gameboard
-{
-string[,] board = new string[6, 7]; // represents the values of each space on the board
-public Gameboard() // constructs an empty gameboard
-{
-for (int i = 0; i <= 5; i++)
-{
-for (int f = 0; f <= 6; f++)
-{
-board[i, f] = " ";
-}
-}
-}*/
+                        // Diagonally left:
+                        try
+                        {
+                            if (board[y + 1, x - 1] == player)
+                            {
+                                if (board[y + 2, x - 2] == player)
+                                    if (board[y + 3, x - 3] == player)
+                                        return true;
+                            }
+                        }
+                        catch (IndexOutOfRangeException) { }
 
 
 
+                        // Diagonally right:
+                        try
+                        {
+                            if (board[y + 1, x + 1] == player)
+                            {
+                                if (board[y + 2, x + 2] == player)
+                                    if (board[y + 3, x + 3] == player)
+                                        return true;
+                            }
+                        }
+                        catch (IndexOutOfRangeException) { }
+                    }
+                }
+            }
 
 
 
-/*internal class Program
-{
-static void Main(string[] args)
-{
-Gameboard gameboard = new Gameboard();
-gameboard.Display();
-Console.ReadKey();
-*/
+            return false;
+        }
 
 
 
-class Program
-{
-static void Main(string[] args)
-{
-GameBoard game = new GameBoard();
+        public bool Full() //returns true if every space on the board is filled
+        {
+            return Count >= rows * columns;
+        }
+    }
 
 
 
-char player = 'X';
-int column;
 
 
 
-bool gameLoop = true;
-bool inputLoop;
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valid = false;
+            String gameType = " "; 
+            Player player1 = new Player();
+            Player player2 = new Player();
+            
+            Console.WriteLine("Welcome\nHow would you like to play?:\n[1]:Player Vs Player\n[2]:Player Vs AI\n[3]:AI Vs AI");
 
+             while (valid == false)
+         {
+                
+                gameType = Console.ReadLine();
 
-while (gameLoop)
-{
 
+                switch (gameType)  //chooses who will be playing
+                {
+                    case "1":
+                        player1 = new Player('X');
+                        player2 = new Player('O');
+                        valid = true;
+                        break;
+                    case "2":
+                        player1 = new Player('X');
+                        player2 = new Ai('O');
+                        valid = true;
+                        break;
+                    case "3":
+                        player1 = new Ai('X');
+                        player2 = new Ai('O');
+                        valid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please choose again");
+                        break;
+                }
 
+         } 
+                GameBoard game = new GameBoard();
 
-System.Console.Clear();
-game.Display();
 
 
+           
+                
+                Player player = player1;
+                int column;
 
-do
-{
-inputLoop = true;
 
 
+                bool gameLoop = true;
+                bool inputLoop;
 
-Console.Write("\nPlayer ");
-Console.Write(player);
-Console.Write(": ");
 
 
+                while (gameLoop) //loops until the game is over
+                {
 
-if (Int32.TryParse(Console.ReadLine(), out column))
-{
-if (1 <= column && column <= 7)
-{
-if (game.DropPiece(player, column))
-{
-inputLoop = false;
-}
-else
-{
-System.Console.Clear();
-game.Display();
-Console.WriteLine("\nFull column");
-}
-}
-else
-{
-System.Console.Clear();
-game.Display();
-Console.WriteLine("\nThe integer must be between 1 and 7.");
-}
-}
-else
-{
-System.Console.Clear();
-game.Display();
-Console.WriteLine("\nPlease enter an integer.");
-}
-} while (inputLoop);
 
 
+                    System.Console.Clear();
+                    game.Display();
 
-if (game.connectRow(player))
-{
-System.Console.Clear();
-game.Display();
-Console.Write("\nPlayer ");
-Console.Write(player);
-Console.Write(" has won!\n");
-Console.WriteLine("\nPress enter to quit.");
-gameLoop = false;
-}
-else if (game.Full())
-{
-System.Console.Clear();
-game.Display();
-Console.WriteLine("\nIt is a draw.");
-Console.WriteLine("\nPress enter to quit.");
-gameLoop = false;
-}
-else
-{
-player = player == 'X' ? 'O' : 'X';
-}
-}
 
 
+                    do
+                    {
+                        inputLoop = true;
 
-Console.ReadKey();
-}
 
 
+                        Console.Write("\nPlayer ");
+                        Console.Write(player.piece);
+                        Console.Write(": ");
 
-}
+
+
+                        if (Int32.TryParse(player.PlayerInput(game.board), out column))  
+                        {
+                            if (1 <= column && column <= 7)
+                            {
+                                if (game.DropPiece(player.piece, column))
+                                {
+                                    if (gameType == "3" || gameType == "2")
+                                    {
+                                    Console.ReadKey();
+                                    }
+                                    inputLoop = false;
+                                }
+                                else
+                                {
+                                    System.Console.Clear();
+                                    game.Display();
+                                    Console.WriteLine("\nFull column");
+                                }
+                            }
+                            else
+                            {
+                                System.Console.Clear();
+                                game.Display();
+                                Console.WriteLine("\nThe integer must be between 1 and 7.");
+                            }
+                        }
+                        else
+                        {
+                            System.Console.Clear();
+                            game.Display();
+                            Console.WriteLine("\nPlease enter a valid integer.");
+                        }
+                    } while (inputLoop);
+                    
+
+
+                    if (game.connectRow(player.piece)) //checks to see if game is won
+                    {
+                        System.Console.Clear();
+                        game.Display();
+                        Console.Write("\nPlayer ");
+                        Console.Write(player.piece);
+                        Console.Write(" has won!\n");
+                        Console.WriteLine("\nPress 1 to play again");
+                        Console.WriteLine("Press 2 to quit.");
+                        if (Console.ReadLine() == "1")
+                        {
+                            game = new GameBoard();
+                        }
+                        else
+                        {
+                            gameLoop = false;
+                        }
+
+                }
+                else if (game.Full()) //checks for a tie
+                    {
+                        System.Console.Clear();
+                        game.Display();
+                        Console.WriteLine("\nIt is a draw.");
+                        Console.WriteLine("\nPress 1 to play again");
+                        Console.WriteLine("Press 2 to quit.");
+                        if (Console.ReadLine() == "1")
+                        {
+                        game = new GameBoard();
+                        }
+                        else
+                        {
+                            gameLoop = false;
+                        } 
+                    
+                    }
+                    else  // switches player turn
+                    {
+                        player = player == player1 ? player2 : player1;
+                    }
+
+                
+                }
+
+              
+
+           
+        }
+
+
+
+    }
 }
